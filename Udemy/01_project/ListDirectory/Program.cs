33de6f0ff45a86e94d4af1d49bfe54cdc4f6fe10
Cristhian Fernández <daniel_24_10@hotmail.com>
2025-08-21 19:09:41 +0200

    ListDirectory added.

---
+++ Udemy/01_project/ListDirectory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace ListDirectory
+{
+    class Program
+    {
+        static void Main()
+        {
+            object[] parametros = { "Alex", 25, true };
+            new Program().metodo(parametros);
+            Console.ReadLine();
+        }
+        private void metodo(object[] parametros)
+        {
+
+        }
+    }
+}
